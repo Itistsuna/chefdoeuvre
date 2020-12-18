e6991cbd76d3496d98f717562e76e218346c8386
Itistsuna <arnaudg9806@gmail.com>
2020-12-18 17:10:10 +0100

    db table

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -10,8 +10,8 @@ const connect = mysql.createConnection({
 connect.connect(function(err){
     if(err) throw err
     let createDB = "CREATE database IF NOT EXISTS chef_d_oeuvre"
-    let productTable = "CREATE TABLE IF NOT EXISTS produits (id_product INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(1000) NOT NULL, description VARCHAR(2000) NOT NULL,prix_ttc INT(255) NOT NULL, stock INT(255) NOT NULL)"
-    let panierTable = "CREATE TABLE IF NOT EXISTS paniers (id_panier INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, id_produit INT(255) NOT NULL)"
+    let productTable = "CREATE TABLE IF NOT EXISTS produits (id_produit INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(1000) NOT NULL, description VARCHAR(2000) NOT NULL,prix_ttc INT(255) NOT NULL, stock INT(255) NOT NULL)"
+    let panierTable = "CREATE TABLE IF NOT EXISTS paniers (id_panier INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, id_produit INT(255) NOT NULL,quantité INT(255) NOT NULL, FOREIGN KEY (id_produit) REFERENCES produits(id_produit))"
     connect.query(createDB, (err)=>{
         if (err) throw err
         else console.log('Database crée');
@@ -19,6 +19,10 @@ connect.connect(function(err){
     connect.query(productTable, (err) => {
         if(err) throw err
         else console.log('Table produit crée');
+    })
+    connect.query(panierTable, (err) => {
+        if (err) throw err
+        else console.log("Table panier crée");
     })
 
 })
